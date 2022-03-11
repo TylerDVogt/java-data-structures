1173628e8af1e347b008c81bcac334fc4ff32d84
Tyler <tylervog@buffalo.edu>
2022-03-11 04:22:32 -0500

    added new methods and documentation to public methods

--- Stack.java
+++ Stack.java
@@ -9,7 +9,12 @@ public class Stack<T> {
 		arr = new Object[DEFAULT_SIZE];
 		this.size = DEFAULT_SIZE;
 	}
-
+	
+	/**
+	 * 
+	 * @param <T> item
+	 * @return <T> item
+	 */
 	public T push(T item) {
 		if(count==size) {
 			grow();
@@ -19,9 +24,18 @@ public class Stack<T> {
 		return item;
 	}
 	
+	/**
+	 * Peeks element on top of stack
+	 * @return Object
+	 */
 	public Object peek() {
 		return arr[count-1];
 	}
+	
+	/**
+	 * Removes the top element and returns that object
+	 * @return Object
+	 */
 	public Object pop() {
 		if(count == 0) {
 			return null;
@@ -30,6 +44,9 @@ public class Stack<T> {
 		return arr[count];
 	}
 	
+	/**
+	 * Sets all current elements to null
+	 */
 	public void clear() {
 		for(int i = 0;i<size;i++) {
 			arr[i] = null;
@@ -37,8 +54,34 @@ public class Stack<T> {
 		count = 0;
 	}
 	
+	/**
+	 * 
+	 * @param <T> item
+	 * @return i - index or -1 if not found
+	 */
+	public int search(T item) {
+		for(int i = 0;i<count;i++) {
+			if(arr[i] == item) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	/**
+	 * 
+	 * @return size of stack
+	 */
 	public int getSize() {
 		return size;
+	}
+	
+	/**
+	 * Displays the contents of the stack
+	 */
+	public void display() {
+		for(int i = 0;i<count;i++) {
+			System.out.println(arr[i]);
+		}
 	}
 
 	private void grow() {
