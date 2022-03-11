9becef574144cf54ad9b89897ed16223ee70e7b7
Tyler <tylervog@buffalo.edu>
2022-03-11 03:53:24 -0500

    Stack

---
+++ Stack.java
@@ -0,0 +1,44 @@
+public class Stack<T> {
+	
+	private static final int DEFAULT_SIZE = 10;
+	private int count;
+	private int size;
+	private Object[] arr;
+	
+	public Stack() {
+		arr = new Object[DEFAULT_SIZE];
+		this.size = DEFAULT_SIZE;
+	}
+	
+	public T push(T item) {
+		if(count==size) {
+			grow();
+		}
+		arr[count] = item;
+		count++;
+		return item;
+	}
+	
+	public Object pop() {
+		if(count == 0) {
+			return null;
+		}
+		count--;
+		return arr[count];
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	private void grow() {
+		size = (int)(size*1.5);
+		Object[] temp = new Object[size];
+		for(int i = 0;i<count;i++) {
+			temp[i] = arr[i];
+		}
+		arr = temp;
+	}
+	
+	
+}
